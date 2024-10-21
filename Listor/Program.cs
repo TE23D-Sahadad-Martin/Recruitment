9b2cb37ae2b46c41081441dda1c013585bb91fdb
martin sahadad <martin.sahadad@elev.ga.ntig.se>
2024-10-21 09:56:02 +0200

    Sista commit

--- Listor/Program.cs
+++ Listor/Program.cs
@@ -1,1 +1,53 @@
-﻿Console.WriteLine("hej"); 
+﻿
+
+        string[] potentiellaRekryter = { "Anna", "Erik", "Maria", "David", "Sara", "Omar", "Lisa" };
+        string[] rekryter = new string[3]; 
+
+        int rekryteradeAntal = 0; 
+
+
+        while (rekryteradeAntal < 3)
+        {
+
+            System.Console.WriteLine("Potentiella rekryter:");
+            for (int i = 0; i < potentiellaRekryter.Length; i++)
+            {
+                if (potentiellaRekryter[i] != null)
+                {
+                    System.Console.WriteLine((i + 1) + ". " + potentiellaRekryter[i]);
+                }
+            }
+
+
+            System.Console.WriteLine("\nAnge numret på personen du vill rekrytera:");
+            int val = int.Parse(System.Console.ReadLine()) - 1;
+
+
+            if (val >= 0 && val < potentiellaRekryter.Length && potentiellaRekryter[val] != null)
+            {
+                rekryter[rekryteradeAntal] = potentiellaRekryter[val];
+                potentiellaRekryter[val] = null; 
+                rekryteradeAntal++;
+
+                System.Console.WriteLine("Du har rekryterat: " + rekryter[rekryteradeAntal - 1]);
+            }
+            else
+            {
+                System.Console.WriteLine("Ogiltigt val, försök igen.");
+            }
+
+
+            System.Console.WriteLine("\nRekryterade personer:");
+            for (int i = 0; i < rekryteradeAntal; i++)
+            {
+                System.Console.WriteLine(rekryter[i]);
+            }
+
+            System.Console.WriteLine("\n");
+        }
+
+        System.Console.WriteLine("Du har rekryterat tre personer!");
+        Console.ReadLine(); 
+    
+
+
